b215fffce58f6160d8e87f7b1761a158cabc3684
Dmitri Kuznetsov <kna_int@mail.ru>
2019-01-13 23:00:44 +0200

    Update MoveRigidBody3DOnKey.cs
    Should be using Time.deltaTime when reading

--- Assets/Scripts/Scripts/System/Physics/Kinematic/Movement/MoveRigidBody3DOnKey.cs
+++ Assets/Scripts/Scripts/System/Physics/Kinematic/Movement/MoveRigidBody3DOnKey.cs
@@ -26,9 +26,9 @@ public class MoveRigidBody3DOnKey : MonoBehaviour
 
             Vector3 position = rb3d.position;
             position.Set(
-                rb3d.position.x + Direction.x * Force * Time.fixedDeltaTime * horizontal,
-                rb3d.position.y + Direction.y * Force * Time.fixedDeltaTime * horizontal,
-                rb3d.position.z + Direction.z * Force * Time.fixedDeltaTime * horizontal);
+                rb3d.position.x + Direction.x * Force * Time.deltaTime * horizontal,
+                rb3d.position.y + Direction.y * Force * Time.deltaTime * horizontal,
+                rb3d.position.z + Direction.z * Force * Time.deltaTime * horizontal);
 
             rb3d.MovePosition(position);
         }
