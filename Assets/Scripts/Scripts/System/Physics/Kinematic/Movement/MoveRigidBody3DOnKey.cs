b842af03e0d02854eb1a7c39f46102f29206ddcb
Dmitri Kuznetsov <kna_int@mail.ru>
2019-01-13 22:35:19 +0200

    Added sample movement code

---
+++ Assets/Scripts/Scripts/System/Physics/Kinematic/Movement/MoveRigidBody3DOnKey.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveRigidBody3DOnKey : MonoBehaviour
+{
+    public float Force;
+    public Vector3 Direction;
+
+    public InputButton Button;
+
+    Rigidbody rb3d;
+
+    private void Start()
+    {
+        rb3d = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    private void FixedUpdate()
+    {
+        if (Input.GetButton(Button.KeyName))
+        {
+            float horizontal = Input.GetAxis(Button.KeyName);
+
+            Vector3 position = rb3d.position;
+            position.Set(
+                rb3d.position.x + Direction.x * Force * Time.fixedDeltaTime * horizontal,
+                rb3d.position.y + Direction.y * Force * Time.fixedDeltaTime * horizontal,
+                rb3d.position.z + Direction.z * Force * Time.fixedDeltaTime * horizontal);
+
+            rb3d.MovePosition(position);
+        }
+
+    }
+}
