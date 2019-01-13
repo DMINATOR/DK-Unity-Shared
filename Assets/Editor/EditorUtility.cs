4f4be5581e61956c102485895d6186e8e61318fa
Dmitri Kuznetsov <kna_int@mail.ru>
2019-01-13 18:15:29 +0200

    Moved over some files to the shared folder

---
+++ Assets/Editor/EditorUtility.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+
+/// <summary>
+/// Definition for the code to be generated
+/// </summary>
+public class EditorCodeGenerationDefinition
+{
+    public string Path;
+
+    public string Name;
+
+    public string Contents;
+}
+
+public class EditorUtilityConstants
+{
+    public const string SCRIPTS_FOLDER = "Assets/Scripts/Generated/";
+}
+
+
+/// <summary>
+/// Utility to use for EDITOR code
+/// </summary>
+public class EditorUtility : MonoBehaviour
+{
+    public static void GenerateClass(EditorCodeGenerationDefinition classDefinition)
+    {
+        var dir = new DirectoryInfo(classDefinition.Path);
+        if( !dir.Exists )
+        {
+            dir.Create();
+        }
+
+        string fullPath = classDefinition.Path + classDefinition.Name + ".cs";
+        if ( File.Exists(fullPath))
+        {
+            File.Delete(fullPath);
+        }
+
+
+        Debug.Log($"Creating Class: {fullPath}");
+
+        File.WriteAllText(fullPath, classDefinition.Contents);
+
+        Debug.Log($"Class: {fullPath} generated");
+
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Scripts/Input/InputMappingKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class InputMappingKey
+{
+    //Display name safe for retrieval
+    public string SafeName;
+    public string Name;
+
+    public string DescriptiveName;
+    public string DescriptiveNegativeName;
+
+    public string NegativeButton;
+    public string PositiveButton;
+
+    public string AltNegativeButton;
+    public string AltPositiveButton;
+}
