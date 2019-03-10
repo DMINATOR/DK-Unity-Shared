7920d811fde483860f0894898914525b03267267
Dmitri Kuznetsov <kna_int@mail.ru>
2019-03-10 23:05:31 +0200

    Save Game controller added

---
+++ Assets/Editor/SaveGameWindow.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SaveGameWindow : EditorWindow
+{
+    public bool Loaded = false;
+
+    public SaveGameData SaveGameData = null;
+
+    Vector2 _scrollPos;
+
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Project/Save Game")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(SaveGameWindow));
+    }
+
+    void OnGUI()
+    {
+        _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, false, false);
+
+        SerializedObject serializedObject = new SerializedObject(this);
+
+        OnInitialLoad();
+
+        OnSaveGameData(serializedObject);
+
+        serializedObject.ApplyModifiedProperties();
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    void OnInitialLoad()
+    {
+        if (!Loaded)
+        {
+            SaveGameData = SaveGameController.Instance.Load();
+            Loaded = true;
+        }
+    }
+
+    void OnSaveGameData(SerializedObject serializedObject)
+    {
+        GUILayout.Label("Save Game Data", EditorStyles.boldLabel);
+
+        if (SaveGameData != null)
+        {
+            SerializedProperty serializedProperty = serializedObject.FindProperty(nameof(SaveGameData));
+            EditorGUILayout.PropertyField(serializedProperty, true);
+
+            if (GUILayout.Button("Save"))
+            {
+                //save values
+                SaveGameController.Instance.Save(SaveGameData);
+            }
+        }
+
+        if (GUILayout.Button("Load"))
+        {
+            SaveGameData = SaveGameController.Instance.Load();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Plugins/Game/SaveGameController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for all save game data
+/// </summary>
+[System.Serializable]
+public partial class SaveGameData : DKAsset
+{
+    public int BaseField;
+}
+
+public class SaveGameController
+{
+    private static SaveGameController _instance;
+    public static SaveGameController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SaveGameController();
+            }
+            //else - instance created, re-use
+
+            return _instance;
+        }
+    }
+
+    public SaveGameData Load()
+    {
+        return AssetLoader.Load<SaveGameData>();
+    }
+
+    public void Save(SaveGameData data)
+    {
+        AssetLoader.Save(data);
+    }
+}
