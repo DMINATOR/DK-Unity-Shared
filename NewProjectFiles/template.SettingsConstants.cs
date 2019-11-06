8f7d031941f1915973f723c3de21cfa985284c0a
Dmitri Kuznetsov <kna_int@mail.ru>
2019-11-06 20:00:32 +0200

    Added new constants

--- NewProjectFiles/template.SettingsConstants.cs
+++ NewProjectFiles/template.SettingsConstants.cs
@@ -13,7 +13,8 @@ public class SettingsConstants
     {
 		MusicVolume,
 		SoundVolume,
-
+		TIME_CONTROL_ELEMENTS_SIZE,
+		TIME_CONTROL_CHANGE_DIFFERENCE,
     }
 
     public static void Load()
@@ -33,6 +34,22 @@ public class SettingsConstants
 			MinValue = "0.0f",
 			DefaultValue = "0.7f",
 			MaxValue = "1.0f"
+		});
+		SettingsController.Instance.AddSetting(new SettingValue()
+		{
+			Name = Enum.GetName(typeof(SettingsConstants.Name), Name.TIME_CONTROL_ELEMENTS_SIZE),
+			Type = SettingValueType.Integer,
+			MinValue = "0",
+			DefaultValue = "100",
+			MaxValue = "10000"
+		});
+		SettingsController.Instance.AddSetting(new SettingValue()
+		{
+			Name = Enum.GetName(typeof(SettingsConstants.Name), Name.TIME_CONTROL_CHANGE_DIFFERENCE),
+			Type = SettingValueType.Float,
+			MinValue = "0",
+			DefaultValue = "0,2",
+			MaxValue = "1"
 		});
 
     }
