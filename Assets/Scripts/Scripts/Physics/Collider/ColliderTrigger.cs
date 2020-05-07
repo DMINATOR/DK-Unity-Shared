fc12011e7dddf33d721e62fb50e887d91cfc40a1
Dmitri Kuznetsov <kna_int@mail.ru>
2020-05-07 19:18:54 +0300

    Something changed

---
+++ Assets/Scripts/Scripts/Physics/Collider/ColliderTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderTrigger : MonoBehaviour
+{
+
+    [Tooltip("Event is triggered when collider is triggered")]
+    public UnityEvent ColliderTriggeredCallback;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        ColliderTriggeredCallback.Invoke();
+    }
+}
